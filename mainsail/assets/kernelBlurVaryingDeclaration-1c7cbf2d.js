579caf6345518943b775c3a82e89798b145608da
gear_geek <gear2nd.droid@gmail.com>
2025-01-22 18:45:57 +0900

    Add mainsail

---
+++ mainsail/assets/kernelBlurVaryingDeclaration-1c7cbf2d.js
@@ -0,0 +1,1 @@
+import{S as r}from"./Viewer-cb1d5b7c.js";const e="kernelBlurVaryingDeclaration",a="varying vec2 sampleCoord{X};";r.IncludesShadersStore[e]=a;
---
+++ mainsail/assets/kernelBlurVaryingDeclaration-88e4db2d.js
@@ -0,0 +1,1 @@
+import{S as r}from"./Viewer-cb1d5b7c.js";const e="kernelBlurVaryingDeclaration",a="varying sampleCoord{X}: vec2f;";r.IncludesShadersStoreWGSL[e]=a;
